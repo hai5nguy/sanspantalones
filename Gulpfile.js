dffb61ddb114c4b66178f8788ecf7175136c4f3e
hnguyen <hnguyen@exacttarget.com>
2015-09-06 12:52:23 -0400

    html watch working

---
+++ Gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var express = require('gulp-express');
+var del = require('del');
+
+
+gulp.task('clean', function (cb) {
+
+    del(['./dist/**', '!./dist']).then(function () {
+        cb();
+    }, function (error) {
+        cb(error);
+    });
+
+
+});
+
+gulp.task('move', [ 'clean' ], function () {
+    return gulp.src('./src/frontend/index.html').pipe(gulp.dest('./dist'));
+});
+
+
+
+gulp.task('server', [ 'move' ], function () {
+    express.run([ './src/backend/server.js' ]);
+
+    gulp.watch([ './dist/**' ], function (event) {
+        if (event && event.type === 'changed') {
+            console.log('File changed: ', event.path);
+            express.notify(event);
+        }
+    });
+});
+
+
+gulp.task('watch:index', function () {
+     var htmlFiles = [
+         './src/frontend/index.html'
+     ];
+     gulp.watch(htmlFiles, ['move']);
+});
+
+
+
+gulp.task('default', ['server', 'watch:index']);
+
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var livereload = require('connect-livereload');
 
 var server = express();
 
 var port = process.env.port|| 5000;
 
+server.use(livereload());
 server.use(express.static(__dirname + '/../../dist/'));
 
 server.listen(port, function () {
