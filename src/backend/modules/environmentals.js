85f97e0bbd9f231398fcbb9aac46cf843bfd0dc2
hnguyen <hnguyen@exacttarget.com>
2015-09-06 14:38:03 -0400

    added environmentals

---
+++ src/backend/modules/environmentals.js
@@ -0,0 +1,3 @@
+global.SP_ENVIRONMENT   = process.env.environment || 'local';             //environment=production node src/backend/server.js
+global.SP_PORT          = process.env.port || 5000;                       //port=8080 node src/backend/server.js
+
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var livereload = require('connect-livereload');
 
+require('./modules/environmentals.js');
+
 var server = express();
 
-var port = process.env.port|| 5000;
+if (SP_ENVIRONMENT === 'local') {
+    server.use(livereload());                   //this must be call before the serving of any static files
+}
 
-server.use(livereload());
 server.use(express.static(__dirname + '/../../dist/'));
 
-server.listen(port, function () {
-    console.log('server running on port ', port);
+server.listen(SP_PORT, function () {
+    console.log('Sans Pantalones server running on port', SP_PORT);
 });
