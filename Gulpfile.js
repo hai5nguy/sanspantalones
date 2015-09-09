c6e6cee8236b9b8657701323b5bbcbce1cf4d792
Hai Nguyen <hai5nguy@gmail.com>
2015-09-09 19:58:54 -0400

    separating gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,86 +1,17 @@
-var gulp        = require('gulp');
-var gls         = require('gulp-live-server');
-var del         = require('del');
-var watch       = require('gulp-watch');
-var sass        = require('gulp-sass');
-var concat      = require('gulp-concat');
-var inject      = require('gulp-inject');
-var htmlReplace = require('gulp-html-replace');
+var gulp = require('gulp');
 
-var config = require('./gulp-config.js');
-
-var server = gls.new(config.server.executable);
-
-/*******************************************************************************/
-
-gulp.task('default', ['server']);
-
-/*******************************************************************************/
-
-gulp.task('server', [ 'build-dist', 'watch' ], function (cb) {
-    server.start().then(function () {
-        cb();
-    }, function (error) {
-        cb(error);
-    });
-});
+require('./gulp-local.js');
+// require('./gulp-dev.js');
+// require('./gulp-qa.js');
+// require('./gulp-staging.js');
+// require('./gulp-prod.js')
 
 
-gulp.task('watch', [ 'build-dist' ], function () {
-
-    watch(config.watch, function (event) {
-        gulp.start('rebuild-dist')
-    });
-
-});
-
-gulp.task('rebuild-dist', [ 'build-dist' ], function (cb) {
-    gulp.src(config.index).pipe(server.notify());
-    cb();
-});
-
-gulp.task('build-dist', [ 'inject', 'move', 'sass' ]);
-
-gulp.task('move', [ 'wipe-dist' ], function () {
-    return gulp
-        .src(config.move.source, { base: config.base })
-        .pipe(gulp.dest(config.dist))
-});
-
-gulp.task('inject', [ 'move', 'inject:livereload' ] );
-
-gulp.task('inject:livereload', [ 'wipe-dist' ], function () {
-
-    var d = new Date();
-    var timestamp = d.valueOf();
-
-    return gulp.src('./src/frontend/index.html')
-        .pipe(htmlReplace({ 'livereload': '//localhost:35729/livereload.js?v=' + timestamp }))
-        .pipe(gulp.dest('./dist/'));
-
-});
-
-gulp.task('sass', [ 'wipe-dist' ], function () {
-    return gulp.src(config.sass.scss)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(config.dist));
-});
+gulp.task('default', ['local']);
 
 
-gulp.task('wipe-dist', function (cb) {
-
-    del(config.del.dist).then(function () {
-        cb();
-    }, function (error) {
-        cb(error);
-    });
-
-});
-
-/*******************************************************************************/
-
-gulp.task('psuh_To_Dev_Becusae_I_Konw_Waht_Im_Diong_And_Hai_Is_A_Dcik_For_Maikng_Me_Cpoy_And_Psate_Tihs', function () {
-    //to be continue...
-});
+// gulp.task('psuh_To_Dev_Becusae_I_Konw_Waht_Im_Diong_And_Hai_Is_A_Dcik_For_Maikng_Me_Cpoy_And_Psate_Tihs', function () {
+//     //to be continue...
+// });
 
 
