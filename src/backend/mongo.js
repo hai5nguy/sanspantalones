fd5170bcad102742f0baf1536c85bfd7f0752357
hnguyen <hnguyen@exacttarget.com>
2015-10-03 18:30:06 -0400

    console log mongo error

--- src/backend/mongo.js
+++ src/backend/mongo.js
@@ -3,6 +3,7 @@ var MongoClient = require('../../node_modules/mongodb').MongoClient;
 MongoClient.connect(SP_DB_CONNECTION_STRING, function (err, db) {
     if (err) {
         console.error('Unable to connect to mongo server.');
+        console.log(err);
     } else {
         console.log('Mongo online:', SP_DB_CONNECTION_STRING);
         global.DB_SERVER = db;
