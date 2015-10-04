998a248d82f9a2b51a34976510a91a43eb16e9df
Hai Nguyen <hai5nguy@gmail.com>
2015-10-04 16:10:55 -0400

    added puppychow chat to all rooms created

--- src/frontend/components/room/room.controller.js
+++ src/frontend/components/room/room.controller.js
@@ -4,9 +4,9 @@
     angular.module('app')
         .controller('RoomController', RoomController);
 
-    RoomController.$inject = [ '$scope', '$element', '$compile', '$stateParams' ];  //this is need for minification to work
+    RoomController.$inject = [ '$scope', '$element', '$compile', '$stateParams', 'MessageService' ];  //this is need for minification to work
 
-    function RoomController($scope, $element, $compile, $stateParams) {
+    function RoomController($scope, $element, $compile, $stateParams, MessageService) {
 
         // console.log($stateParams);
         // debugger;
@@ -17,7 +17,49 @@
         //     var llama = $compile('<sp-component-llama></sp-component-llama>')($scope.$new());
         //     $element.find('#llama-container').append(llama);
         // }
+        // 
+        
+        loadChatLog();
 
-    }
+        $scope.onChatKeyPress = onChatKeyPress;
+
+        function onChatKeyPress(e) {
+            if (e.keyCode == 13) {
+
+                var chatInput = $element.find('.chatinput');
+                var chatText = chatInput.val();
+                chatInput.val('');
+                
+                MessageService.post({ text: chatText }).then(function (result) {
+                    // var savedMessage = result.data;
+                    // var messageHtml = '<p><span>' + savedMessage._id + '</span> : <span>' + savedMessage.message + '</span></p>';
+                    // $('.chatlog').append($(messageHtml));
+                    // 
+                    loadChatLog();
+                    
+                }, function (error) {
+                //     var messageHtml = '<p>Something went terribly wrong with message posting</p>';
+                //     $('.chatlog').append($(messageHtml));
+                    console.error(error);
+                });
+            }
+        }
+
+        function loadChatLog() {
+
+            MessageService.get({ page: 1, size: 50 }).then(function (messages) {
+                // debugger;
+                $scope.messages = messages.reverse();
+            }, function (error) {
+                // debugger;
+                console.error('can not get chatlog');
+            });
+        }
+
+
+
+    } //RoomController
+
+
 
 })();
