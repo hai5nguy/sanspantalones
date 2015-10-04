94b33431083427406c3f63605010ca7ad0dc4f99
Hai Nguyen <hai5nguy@gmail.com>
2015-10-04 11:53:04 -0400

    gtimestamp: Sun Oct  4 11:53:04 EDT 2015

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,7 @@ require('del');
 
 gulp.task('default', startLocalEnvironment);
 
-// gulp.task('build-dev', buildDev);
-
-gulp.task('build-environment', buildEnvironment)
+gulp.task('build-environment', buildEnvironment);   //this will be ran by heroku, leave alone
 
 function startLocalEnvironment(cb) {
     require('./gulp/local/tasks.js');
@@ -13,18 +11,19 @@ function startLocalEnvironment(cb) {
     cb();
 }
 
-// function buildDev(cb) {
-//     require('./gulp/build-dev.js');
-//     gulp.start('build-for-sanspantalonesdev');
-//     cb();
-// }
-
 function buildEnvironment(cb) {
-    if (process.env.environment === 'dev') {
+    var environment = process.env.environment;
+    if (environment === 'dev') {
         require('./gulp/build-dev.js');
         gulp.start('build-for-sanspantalonesdev', function () {
             cb();
         });
+    } else if (environment === 'qa') {
+        //build for qa, final prod code, needs to match production or staging.  data is the same as dev data
+    } else if (environment === 'staging') {
+        //build for staging, final prod code, and sample production data
+    } else if (environment === 'prod') {
+        //build for production, final live prod code, and live prod data
     }
 }
 
